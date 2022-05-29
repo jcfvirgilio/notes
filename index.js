1352da8a85ebe73c2014154cd2c83f337a5ae411
jcfuentes <jcfvirgilio@gmail.com>
2022-05-29 17:30:52 -0500

    se puso js-heroku

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cors = require('cors')
 
 const app = express()
 let notes = [
-    { id: 1, content: "HTML is easy : server-HEROKU ", date: "2019-05-30T17:30:31.098Z", important: true },
-    { id: 2, content: "Browser can execute only Javascript", date: "2019-05-30T18:39:34.091Z", important: false },
+    { id: 1, content: "HTML is easy : server-HEROKU", date: "2019-05-30T17:30:31.098Z", important: true },
+    { id: 2, content: "Browser can execute only Javascript - HEROKU", date: "2019-05-30T18:39:34.091Z", important: false },
     { id: 3, content: "GET and POST are the most important methods of HTTP protocol", date: "2019-05-30T19:20:14.298Z", important: true }]
 
 
